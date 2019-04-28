a559d887016609a5ef860c393c71d81ebf147f47
Daniel Bubeníček <xbuben05@vutbr.cz>
2019-04-28 21:33:58 +0200

    sachovnice na pevno

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,51 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable
+{
+
+    @FXML
+    private ImageView obr1;
+    @FXML
+    private ImageView black;
+
+    @FXML
+    private ImageView white;
+
+    @FXML
+    private GridPane grid;
+/*
+    @FXML
+    public void initialize()
+    {
+        Image image = new Image("WhitePawn.png");
+        ivnn.setImage(image);
+    }
+    */
+    @FXML
+    public void handleButtonClick(ActionEvent actionEvent)
+    {
+        Image image = new Image("WhitePawn.png");
+        obr1.setImage(image);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        System.out.println("blabla");
+        white.setImage(new Image("WhitePawn.png"));
+        black.setImage(new Image("WhitePawn.png"));
+        BackgroundImage bi = new BackgroundImage(new Image("whiteField.png"), BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        grid.setBackground(new Background(bi));
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,18 +4,43 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setMinHeight(600);
-        primaryStage.setMinWidth(900);
-        primaryStage.setScene(new Scene(root, 900, 600));
+    public void start(Stage primaryStage) throws Exception
+    {
+        /*GridPane gridPane = new GridPane();
+        gridPane.setVgap(8);
+        gridPane.setHgap(10);
+
+        Button button1 = new Button("pokus");
+        GridPane.setConstraints(button1, 0,0);
+
+        Button button2 = new Button("pokus2");
+        GridPane.setConstraints(button2, 1,0);
+
+        Parent board = FXMLLoader.load(getClass().getResource("boardGUI.fxml"));
+        GridPane.setConstraints(board, 1,1);
+
+        gridPane.getChildren().addAll(button1, button2, board);
+
+
+        primaryStage.setScene(new Scene(gridPane, 900, 600));
         primaryStage.show();
+*/
+
+        Parent root = FXMLLoader.load(getClass().getResource("boardGUI.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 800, 800));
+        primaryStage.show();
+
     }
 
 
